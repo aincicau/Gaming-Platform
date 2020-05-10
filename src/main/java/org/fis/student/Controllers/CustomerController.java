28aaa527c95b43c8be62ac082e30f16795f36f8a
Izabella <izabella.kovacs23@gmail.com>
2020-05-10 18:23:56 +0100

    GP-1 Added back button

---
+++ src/main/java/org/fis/student/Controllers/CustomerController.java
@@ -0,0 +1,30 @@
+package org.fis.student.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CustomerController {
+    @FXML
+    private Label id;
+
+    @FXML
+    public void butonBack()
+    {
+        try
+        {
+            Stage stage=(Stage)id.getScene().getWindow();
+            Parent ceva = FXMLLoader.load(getClass().getClassLoader().getResource("Choice.fxml"));
+            stage.setTitle("Choice");
+            stage.setScene(new Scene(ceva,600,600));
+
+        }catch(IOException e) {
+            System.out.println(e);
+        }
+    }
+}
