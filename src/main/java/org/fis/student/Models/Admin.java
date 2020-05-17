a6401b6fbb4bad0ca4bc4b59b2b608bf85fb592a
Izabella <izabella.kovacs23@gmail.com>
2020-05-17 10:28:10 +0100

    GP-1 Added models for customer and administrator

---
+++ src/main/java/org/fis/student/Models/Admin.java
@@ -0,0 +1,56 @@
+package org.fis.student.Models;
+
+import java.util.Objects;
+
+public class Admin {
+    private String username;
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Admin admin = (Admin) o;
+        if(!username.equals(admin.username))
+            return false;
+        return password.equals(admin.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result=username.hashCode();
+        return 31*result + password.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
