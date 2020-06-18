984730091c1c9f0711b0b9fdcc64dd979cc6dbc6
Izabella <izabella.kovacs23@gmail.com>
2020-06-18 12:54:22 +0100

    GP-31 Window implementation

---
+++ src/main/java/org/fis/student/Controllers/RegisterController.java
@@ -0,0 +1,23 @@
+package org.fis.student.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+    @FXML
+    private TextField idField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private ChoiceBox role;
+
+    public void backButton(){
+
+    }
+
+    public void register(){
+
+    }
+}
