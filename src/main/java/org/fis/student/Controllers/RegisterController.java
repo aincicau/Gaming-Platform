42bc9ca6351bbbb78fe8aa5e3d89f6f27288eb72
Izabella <izabella.kovacs23@gmail.com>
2020-06-18 13:03:23 +0100

    GP-31 Back button function implemented

--- src/main/java/org/fis/student/Controllers/RegisterController.java
+++ src/main/java/org/fis/student/Controllers/RegisterController.java
@@ -1,9 +1,15 @@
 package org.fis.student.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class RegisterController {
     @FXML
@@ -14,7 +20,16 @@ public class RegisterController {
     private ChoiceBox role;
 
     public void backButton(){
+        try
+        {
+            Stage stage=(Stage)role.getScene().getWindow();
+            Parent ceva = FXMLLoader.load(getClass().getClassLoader().getResource("Choice.fxml"));
+            stage.setTitle("Choice");
+            stage.setScene(new Scene(ceva,600,600));
 
+        }catch(IOException e) {
+            System.out.println(e);
+        }
     }
 
     public void register(){
